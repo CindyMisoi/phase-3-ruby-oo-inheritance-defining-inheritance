6097b2dffa5f21c7374a97832e050deaf3494a78
CindyMisoi <crmisoi2k@gmail.com>
2023-02-16 15:45:56 +0300

    done

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,16 @@
 class Vehicle
+    # getter and setter => macros
+    attr_accessor :wheel_size, :wheel_number
+    def initialize(wheel_size, wheel_number)
+        @wheel_size = wheel_size
+        @wheel_number = wheel_number
+    end
 
+    # behaviors
+    def go
+        "vrrrrrrrooom!"
+    end
+    def fill_up_tank
+        "filling up!"
+    end
 end
